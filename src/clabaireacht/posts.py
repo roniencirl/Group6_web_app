fc1f03770c0bd03bffd940ff771de89b3d539dd3
Ronan Shaw <x22129618@student.ncirl.ie>
2022-12-06 17:50:43 +0000

    add a post submission function

--- src/clabaireacht/posts.py
+++ src/clabaireacht/posts.py
@@ -1,4 +1,13 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    current_app,
+)
 
 from clabaireacht.database import get_database
 
@@ -17,3 +26,36 @@ def index():
         " ORDER BY created DESC"
     ).fetchall()
     return render_template("posts/index.html", posts=posts)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+# @login_required
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        # TODO:  Sanitise the form input.
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            database = get_database()
+
+            if current_app.config["SECRET_KEY"] == "dev" and "user" not in g:
+                user_id = "testing"
+            else:
+                user_id = g.user["id"]
+
+            database.execute(
+                "INSERT INTO posts (title, body, author_id)" " VALUES (?, ?, ?)",
+                (title, body, user_id),
+            )
+            database.commit()
+            return redirect(url_for("posts.index"))
+
+    return render_template("posts/create.html")
